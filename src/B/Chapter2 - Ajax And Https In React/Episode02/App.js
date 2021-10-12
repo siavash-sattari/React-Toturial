b46aeb5cb4009161bb8ac23d90733ec421dbfad0
siavash sattari <siavash.sattari.75@gmail.com>
2021-10-12 18:02:57 +0330

    B-Ch2-E02

---
+++ src/B/Chapter2 - Ajax And Https In React/Episode02/App.js
@@ -0,0 +1,33 @@
+import React, {Component} from "react";
+
+class App extends Component {
+  state = {
+    postData: [],
+  };
+
+  getPost = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  };
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => this.setState({postData: json}));
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.getPost}>Get Post</button>
+        <h1>Posts :</h1>
+        {this.state.postData.map((post) => (
+          <p key={post.id}>{post.title}</p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
 import React from "react";
 import {render} from "react-dom";
 
-// ================ Chapter1-Basic ================
+// ================ Resource A | Chapter1-Basic ================
 
-// import App from "./Chapter1-Basic/Episode1/App";
-// import App from "./Chapter1-Basic/Episode2/App";
-// import App from "./Chapter1-Basic/Episode3/App";
-// import App from "./Chapter1-Basic/Episode4/App";
-// import App from "./Chapter1-Basic/Episode5/App";
-// import App from "./Chapter1-Basic/Episode6/App";
+// import App from "./A/Chapter1-Basic/Episode01/App";
+// import App from "./A/Chapter1-Basic/Episode02/App";
+// import App from "./A/Chapter1-Basic/Episode03/App";
+// import App from "./A/Chapter1-Basic/Episode04/App";
+// import App from "./A/Chapter1-Basic/Episode05/App";
+// import App from "./A/Chapter1-Basic/Episode06/App";
 
-// ============= Chapter2-LifeCycles ==============
+// ============= Resource A | Chapter2-LifeCycles ===============
 
-// import App from "./Chapter2-LifeCycles/Episode1/App"
-// import App from "./Chapter2-LifeCycles/Episode2/App"
-// import App from "./Chapter2-LifeCycles/Episode3/App"
-// import App from "./Chapter2-LifeCycles/Episode4/App"
+// import App from "./A/Chapter2-LifeCycles/Episode01/App"
+// import App from "./A/Chapter2-LifeCycles/Episode02/App"
+// import App from "./A/Chapter2-LifeCycles/Episode03/App"
+// import App from "./A/Chapter2-LifeCycles/Episode04/App"
 
-// ============= Chapter5-Routing ==============
+// ============= Resource A | Chapter5-Routing ==================
 
-// import App from "./Chapter5-Routing/Episode4/App"
-// import App from "./Chapter5-Routing/Episode5/App"
-// import App from "./Chapter5-Routing/Episode6/App"
-// import App from "./Chapter5-Routing/Episode10/App"
-// import App from "./Chapter5-Routing/Episode11/App"
-// import App from "./Chapter5-Routing/Episode14/App"
-// import App from "./Chapter5-Routing/Episode15/App"
-// import App from "./Chapter5-Routing/Episode16/App"
-import App from "./Chapter5-Routing/Episode17/App"
+// import App from "./A/Chapter5-Routing/Episode04/App"
+// import App from "./A/Chapter5-Routing/Episode05/App"
+// import App from "./A/Chapter5-Routing/Episode06/App"
+// import App from "./A/Chapter5-Routing/Episode10/App"
+// import App from "./A/Chapter5-Routing/Episode11/App"
+// import App from "./A/Chapter5-Routing/Episode14/App"
+// import App from "./A/Chapter5-Routing/Episode15/App"
+// import App from "./A/Chapter5-Routing/Episode16/App"
+// import App from "./A/Chapter5-Routing/Episode17/App"
+
+// ============= Resource B | Chapter5-Routing ==================
+
+import App from "./B/Chapter2 - Ajax And Https In React/Episode02/App";
 
 render(<App />, document.getElementById("root"));
