128a1578aed46b71142c0b823fd5ce853a85f4f8
siavash sattari <siavash.sattari.75@gmail.com>
2021-10-21 18:15:46 +0330

    B-Ch4-E15

--- src/index.js
+++ src/index.js
@@ -79,6 +79,7 @@ import {render} from "react-dom";
 // import App from "./B/Chapter4 - React Hooks/Episode11/App";
 // import App from "./B/Chapter4 - React Hooks/Episode12/App";
 // import App from "./B/Chapter4 - React Hooks/Episode13/App";
-import App from "./B/Chapter4 - React Hooks/Episode14/App";
+// import App from "./B/Chapter4 - React Hooks/Episode14/App";
+import App from "./B/Chapter4 - React Hooks/Episode15/App";
 
 render(<App />, document.getElementById("root"));
