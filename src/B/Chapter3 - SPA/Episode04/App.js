d10a09d5c7d09d5fb6e803e2cd91fd0411ee6dbb
siavash sattari <siavash.sattari.75@gmail.com>
2021-10-22 21:57:40 +0330

    B-Ch3-E04

---
+++ src/B/Chapter3 - SPA/Episode04/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import {BrowserRouter, Route, Switch, Link} from "react-router-dom";
+
+import HomePage from "./Components/HomePage";
+import Blogs from "./Components/Blogs";
+import Products from "./Components/Products";
+import AboutUs from "./Components/AboutUs";
+import Navbar from "./Components/Navbar";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+        <div>
+          <Switch>
+            {/* <Route path="/blogs" render={() => <Blogs name="John" />} /> */}
+            <Route path="/blogs" render={(props) => <Blogs name="John" {...props} />} />
+            <Route path="/products" component={Products} />
+            <Route path="/about" component={AboutUs} />
+            <Route path="/" component={HomePage} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
