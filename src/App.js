b3a5f5ad66b87a0ab3e68f7a1dfdf1e550b8ef24
siavash sattari <siavash.sattari.75@gmail.com>
2022-07-10 06:27:23 -0700

    Contact & Contacts component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/chapter01/Navbar';
+import Contacts from './components/chapter01/contact/Contacts';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Contacts />
     </>
   );
 }
---
+++ src/components/chapter01/contact/Contacts.jsx
@@ -0,0 +1,30 @@
+import Contact from './Contact';
+import { PINK } from '../../../helpers/colors';
+
+const Contacts = () => {
+  return (
+    <>
+      <section className='container'>
+        <div className='grid'>
+          <div className='row'>
+            <div className='col'>
+              <p className='h3'>
+                <button className='btn mx-2 my-3' style={{ backgroundColor: PINK }}>
+                  ساخت مخاطب جدید
+                  <i className='fa fa-plus-circle mx-2' />
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='container'>
+        <div className='row'>
+          <Contact />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Contacts;
